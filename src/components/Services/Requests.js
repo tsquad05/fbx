9fa3301b2f1f31c2fadfd3e8f5abd55dfa4befa0
Okereke Clement Kalu <okerekeclementkalu@gmail.com>
2023-06-23 23:21:36 -0700

    0000

---
+++ src/components/Services/Requests.js
@@ -0,0 +1,39 @@
+const Request = [
+  {
+    id: Math.floor(Math.random() * 109910921919) + 1,
+    thumbnail: "../../images/Business.jpg",
+    rating: 1,
+    avatar: "../../images/person_1-min.jpg",
+    userName: "qhaqha",
+    details: "I can design a logo for your brand in less than4 days",
+  },
+  {
+    id: Math.floor(Math.random() * 109910921919) + 1,
+    thumbnail: "../../images/Business2.jpeg",
+    rating: 2,
+    avatar: "../../images/person_2-min.jpg",
+    userName: "beneathrain",
+    details:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, animi. ",
+  },
+  /*  {
+    id: Math.floor(Math.random() * 109910921919) + 1,
+    thumbnail: "../../images/Business3.jpg",
+    rating: 3,
+    avatar: "../../images/person_3-min.jpg",
+    userName: "iamdevs",
+    details:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, animi.",
+  },
+  {
+    id: Math.floor(Math.random() * 109910921919) + 1,
+    thumbnail: "../../images/Business.jpg",
+    rating: "Top Rating",
+    avatar: "../../images/person_4-min.jpg",
+    userName: "loremiqua",
+    details:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, animi.",
+  }, */
+];
+
+export default Request;
--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,9 +1,31 @@
 import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.css";
 import Aos from "aos";
 import "aos/dist/aos.css";
 import "./Services.css";
+import Requests from "./Requests.js";
 function Services() {
+  let RequestSlider = Requests.map((req) => {
+    return (
+      <div key={req.id} className="sliderContainer">
+        <div className="thumbnailCon">
+          <img src={req.thumbnail} alt="ThumbNail" />
+        </div>
+        <div className="sellerDetails">
+          <img src={req.avatar} alt="Avatar" />
+          <span>{req.userName}</span>
+          <span className="rating">
+            {typeof req.rating === "number"
+              ? "Level" + req.rating
+              : "Top Rated"}
+          </span>
+        </div>
+        <div className="details">{req.details}</div>
+        <button>See Details</button>
+      </div>
+    );
+  });
   useEffect(() => {
     Aos.init();
   }, []);
@@ -13,8 +35,15 @@ function Services() {
       <div className="HeroContainer">
         <div className="heroText">
           <h1>Services</h1>
-          <span>Home / Services</span>
+          <span>
+            <Link to="/">Home</Link> / <span>Services</span>
+          </span>
         </div>
+      </div>
+      <div className="featuredProperties">
+        <div className="featuredProperties"></div>
+        <h2>Featured Properties</h2>
+        <div className="requestContainer">{RequestSlider}</div>
       </div>
     </>
   );
